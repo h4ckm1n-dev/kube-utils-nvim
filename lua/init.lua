456d70a69df863ea0170935a3fe392910c502ba4
h4ckm1n-dev <mayer.julien22@gmail.com>
2024-05-16 23:14:53 +0200

    test

--- lua/init.lua
+++ lua/init.lua
@@ -1,20 +1,22 @@
--- init.lua
+-- /home/h4ckm1n/Documents/Divers/kube-utils-nvim/init.lua
 local M = {}
 
-local Helm = require("modules.helm")
-local Kubectl = require("modules.kubectl")
-local K9s = require("modules.k9s")
+local Helm = require("kube-utils-nvim.modules.helm")
+local Kubectl = require("kube-utils-nvim.modules.kubectl")
+local K9s = require("kube-utils-nvim.modules.k9s")
 
 function M.setup()
-    vim.api.nvim_create_user_command("HelmDeployFromBuffer", Helm.deploy_from_buffer, {})
-    vim.api.nvim_create_user_command("RemoveDeployment", Kubectl.remove_deployment, {})
-    vim.api.nvim_create_user_command("HelmDryRun", Helm.dryrun_from_buffer, {})
-    vim.api.nvim_create_user_command("KubectlApplyFromBuffer", Kubectl.apply_from_buffer, {})
-    vim.api.nvim_create_user_command("DeleteNamespace", Kubectl.delete_namespace, {})
-    vim.api.nvim_create_user_command("HelmDependencyUpdateFromBuffer", Helm.dependency_update_from_buffer, {})
-    vim.api.nvim_create_user_command("HelmDependencyBuildFromBuffer", Helm.dependency_build_from_buffer, {})
-    vim.api.nvim_create_user_command("OpenK9s", K9s.open, {})
-    vim.api.nvim_create_user_command("OpenK9sSplit", K9s.open_split, {})
+    vim.api.nvim_create_user_command("HelmDeployFromBuffer", function() Helm.deploy_from_buffer() end, {})
+    vim.api.nvim_create_user_command("RemoveDeployment", function() Kubectl.remove_deployment() end, {})
+    vim.api.nvim_create_user_command("HelmDryRun", function() Helm.dryrun_from_buffer() end, {})
+    vim.api.nvim_create_user_command("KubectlApplyFromBuffer", function() Kubectl.apply_from_buffer() end, {})
+    vim.api.nvim_create_user_command("DeleteNamespace", function() Kubectl.delete_namespace() end, {})
+    vim.api.nvim_create_user_command("HelmDependencyUpdateFromBuffer",
+        function() Helm.dependency_update_from_buffer() end, {})
+    vim.api.nvim_create_user_command("HelmDependencyBuildFromBuffer", function() Helm.dependency_build_from_buffer() end,
+        {})
+    vim.api.nvim_create_user_command("OpenK9s", function() K9s.open() end, {})
+    vim.api.nvim_create_user_command("OpenK9sSplit", function() K9s.open_split() end, {})
 end
 
 return M
