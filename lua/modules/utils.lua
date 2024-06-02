c0b6d09a131240dbc49a415d3b887030508bd5fc
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2024-06-03 00:15:11 +0200

    cleaner error messages

--- lua/modules/utils.lua
+++ lua/modules/utils.lua
@@ -4,7 +4,7 @@ local Utils = {}
 
 -- Function log_error
 function Utils.log_error(message)
-	print("Error: " .. message)
+	vim.notify("Error: " .. message, vim.log.levels.ERROR)
 end
 
 return Utils
